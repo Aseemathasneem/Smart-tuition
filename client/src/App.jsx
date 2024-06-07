f21117336c1b02f242b70d06476279636c773e7d
Aseema thasneem <aseemathasneem@gmail.com>
2024-06-07 23:40:38 +0530

    Make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-import Student from './pages/Student'
+import Student from './pages/Dashboard'
 import OtpVerification from './pages/OtpVerification'
 import Header from './components/Header'
 import Tutor from './pages/Tutor'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
+import Dashboard from './pages/Dashboard'
 
 
 export default function App() {
@@ -19,6 +21,9 @@ export default function App() {
       <Route path='/sign-in'element={<SignIn/>}/>
       <Route path='/otp-verification' element={<OtpVerification />} />
       <Route path='/sign-up'element={<SignUp/>}/>
+      <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
       <Route path='/student'element={<Student/>}/>
       <Route path='/tutor'element={<Tutor/>}/>
      
