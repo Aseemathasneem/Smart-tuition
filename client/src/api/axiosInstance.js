cdd8aa068bc70a544d5261881cb387d6c54cd025
Aseema thasneem <aseemathasneem@gmail.com>
2024-09-03 09:20:58 +0530

    cors error

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios without setting a default baseURL
 const axiosInstance = axios.create({
-  baseURL: 'http://13.127.132.140/api',
+  baseURL: '/api',
   
 });
 
