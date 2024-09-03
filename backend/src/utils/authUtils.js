8a2d05c8ec39550f60558ebb679e6fe330a02809
Aseema thasneem <aseemathasneem@gmail.com>
2024-09-03 23:42:19 +0530

    fixing error

--- backend/src/utils/authUtils.js
+++ backend/src/utils/authUtils.js
@@ -194,7 +194,7 @@ export async function signin(Model, req, res, next) {
       .cookie(cookieName, accessToken, {
         httpOnly: true,
         secure: true,
-        sameSite: 'None', // Allows cookies to be sent cross-site
+        sameSite: 'Lax', // Allows cookies to be sent cross-site
     domain: '.smarttuition.solutions', // Specify the domain if needed
       })
       
