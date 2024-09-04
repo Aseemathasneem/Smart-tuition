33e5868a2caa17af3ee9fabec5cac04923ac973a
Aseema thasneem <aseemathasneem@gmail.com>
2024-09-04 20:14:43 +0530

    cookies issue

--- backend/src/utils/authUtils.js
+++ backend/src/utils/authUtils.js
@@ -194,8 +194,8 @@ export async function signin(Model, req, res, next) {
       .cookie(cookieName, accessToken, {
         httpOnly: true,
         secure: true,
-    //     sameSite: 'Lax', // Allows cookies to be sent cross-site
-    // domain: '.smarttuition.solutions', // Specify the domain if needed
+        sameSite: 'Lax', // Allows cookies to be sent cross-site
+    domain: '.smarttuition.solutions', // Specify the domain if needed
       })
       
       .json({
