579266a21ad5bfb320428c6264e66bd0c09a16cc
Aseema thasneem <aseemathasneem@gmail.com>
2024-09-04 09:21:24 +0530

    cors issue

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
-const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://smarttuition.solutions/api' 
-  : '/api';
 
 // Create an instance of axios without setting a default baseURL
 const axiosInstance = axios.create({
-  baseURL: baseURL,
+  
   
 });
 
